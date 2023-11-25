6c24577633669bb0d139e7f2309c323fdc2fe675
d.pechenenko <pechenenkod@gmail.com>
2023-11-25 11:30:25 +0100

    Initial Commit

---
+++ Todo List App/Todo_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_AppApp.swift
+//  Todo List App
+//
+//  Created by Dmitro Pechenenko on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
